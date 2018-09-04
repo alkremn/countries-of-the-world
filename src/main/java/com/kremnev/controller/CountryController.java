f1fc86995714eed1cd747e8039f2ea1cb820302c
alkremn <alkremn@gmail.com>
2018-09-04 16:28:40 -0600

    sortBy method added.

--- src/main/java/com/kremnev/controller/CountryController.java
+++ src/main/java/com/kremnev/controller/CountryController.java
@@ -1,11 +1,16 @@
 package com.kremnev.controller;
 
 import com.kremnev.data.CountryRepository;
+import com.kremnev.model.Country;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 
 @Controller
@@ -15,21 +20,33 @@ public class CountryController {
     private CountryRepository countryRepository;
 
     @RequestMapping("/")
-    public String listCountries(ModelMap modelMap){
+    public String listCountries(ModelMap modelMap) {
         modelMap.put("countries", countryRepository.getAllCountries());
         return "index";
     }
 
     @RequestMapping("/countries/{name}")
-    public String listCountryInfo(@PathVariable String name, ModelMap modelMap){
+    public String listCountryInfo(@PathVariable String name, ModelMap modelMap) {
         modelMap.put("country", countryRepository.findCountryByName(name));
         return "country";
     }
 
     @RequestMapping("index.html")
-    public String showMainPage(){
+    public String showMainPage() {
         return "redirect:/";
     }
 
+    @RequestMapping("/sortby/{type}")
+    public String sortBy(@PathVariable String type, ModelMap modelMap) {
+        List<Country> countries = countryRepository.getAllCountries();
+        if (type.equals("name")) {
+            countries.sort(Comparator.comparing(Country::getName));
+        } else if (type.equals("population")) {
+            countries.sort(Comparator.comparing(Country::getPopulationInt));
+            Collections.reverse(countries);
+        }
+        modelMap.put("countries", countries);
+        return "index";
+    }
 
 }
